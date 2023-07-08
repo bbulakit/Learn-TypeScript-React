c4036c537fb16cad3029d7b5e179546d2f745411
equinoxza_57@hotmail.com <b.bulakit@hotmail.com>
2023-07-08 09:16:24 +0700

    Updated Arrow Function, void and never types

--- 02BASICS/myFunction.ts
+++ 02BASICS/myFunction.ts
@@ -8,7 +8,6 @@ return val.toUpperCase();
 
 function signUpUser(name:string, email:string, isPaid:boolean){}
 
-//arrow function
 let loginUser = (name:string, email:string, isPaid:boolean=false) => {}
 
 addTwo(3)
@@ -17,5 +16,26 @@ getUpper("hello");
 
 signUpUser("bulakit", "mymail@email.com", true);
 loginUser("bulakit", "mymail@email.com", false);
+
+//arrow function
+const getHello = (s: string):string => {
+    return "Hello";
+}
+
+const herosArray = ["Peter Parker", "Miles Morales", "Gwen Stacy"];
+//map return entirely new array, whereas forEach() doesn't
+herosArray.map((hero):string => {
+    return `hero is ${hero}`;
+})
+
+//void return nothing while never throws an exception
+function consoleError(errmsg:string):void {
+    console.log(errmsg);
+    // return ""; //error because we set the return type as void
+}
+
+function handleError(errmsg:string):never{
+    throw new Error(errmsg); // need throw exception for "never" type
+}
 
 export{}

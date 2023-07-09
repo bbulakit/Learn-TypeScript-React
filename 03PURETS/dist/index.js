4779d1aa79d89310a3994d31f100a82f98189834
equinoxza_57@hotmail.com <b.bulakit@hotmail.com>
2023-07-09 11:06:55 +0700

    Learning basic class

--- 03PURETS/dist/index.js
+++ 03PURETS/dist/index.js
@@ -1,3 +1,23 @@
 "use strict";
-console.log("TypeScript is here");
-console.log("World, hello!");
+class User {
+    //constructor
+    constructor(email, name) {
+        this.city = ""; //it need to be intialized if not in constructor
+        this.email = email;
+        this.name = name;
+    }
+}
+const user1 = new User("user1@user.com", "user1xx");
+user1.city = "Bangkok";
+// console.log(user1.name)
+//better way of class declaration
+class User2 {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "";
+        this.email = email;
+        this.name = name;
+    }
+}
+const user2 = new User2("user2@user.com", "user2zz");
--- 03PURETS/src/index.ts
+++ 03PURETS/src/index.ts
@@ -1,2 +1,30 @@
-console.log("TypeScript is here");
-console.log("World, hello!");
+class User{
+    //initialize private variables
+    private email:string;
+    public readonly name:string;
+    city:string = ""; //it need to be intialized if not in constructor
+    //constructor
+    constructor(email:string, name:string){
+        this.email = email;
+        this.name = name;
+    }
+}
+
+const user1 = new User("user1@user.com", "user1xx")
+user1.city = "Bangkok"
+// console.log(user1.name)
+
+//better way of class declaration
+class User2{
+    city:string = "";
+    constructor(
+        private email:string, 
+        public readonly name:string
+        ){
+            this.email = email;
+            this.name = name;                        
+
+    }
+}
+
+const user2 = new User2("user2@user.com", "user2zz")

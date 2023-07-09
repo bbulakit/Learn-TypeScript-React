3c59acb1065d61c96d0d3059fb207f1022070565
equinoxza_57@hotmail.com <b.bulakit@hotmail.com>
2023-07-09 21:43:04 +0700

    Added generic class example

--- 03PURETS/dist/myGenerics.js
+++ 03PURETS/dist/myGenerics.js
@@ -29,3 +29,27 @@ const GetMoreSearchProducts = (products) => {
     const returnIndex = 2;
     return products[returnIndex];
 };
+//generic class
+//purpose is, sometimes we wouldn't know about data type because it's depending on user
+//method invoker/caller or some kind like that will define the data type himself
+//improve performance in case of using any/object type which the data type will be converted to object before and aftter
+//more flexible and reusable
+function AnotherFunction(valOne, valTwo) {
+    return { valOne, valTwo };
+}
+// AnotherFunction(3,"3") // it's worked before U extends number
+AnotherFunction(3, 4.6);
+function AnotherFunction2(valOne, valTwo) {
+    return { valOne, valTwo };
+}
+AnotherFunction2(3, { connection: "localhost", username: "123", password: "123" });
+const testDatabase = { connection: "localhost", username: "test", password: "test" };
+AnotherFunction2(1234, testDatabase);
+class Sellable {
+    constructor() {
+        this.cart = [];
+        this.addToCart = (products) => {
+            this.cart.push(products);
+        };
+    }
+}
--- 03PURETS/src/myGenerics.ts
+++ 03PURETS/src/myGenerics.ts
@@ -46,3 +46,50 @@ const GetMoreSearchProducts = <T>(products: T[]): T => {
     const returnIndex = 2
     return products[returnIndex]
 }
+
+
+//generic class
+//purpose is, sometimes we wouldn't know about data type because it's depending on user
+//method invoker/caller or some kind like that will define the data type himself
+//improve performance in case of using any/object type which the data type will be converted to object before and aftter
+//more flexible and reusable
+function AnotherFunction<T, U extends number>(valOne:T, valTwo:U):object {
+    return {valOne, valTwo}
+}
+
+// AnotherFunction(3,"3") // it's worked before U extends number
+AnotherFunction(3,4.6);
+
+interface Database{
+    connection:string,
+    username:string,
+    password:string
+}
+
+function AnotherFunction2<T, U extends Database>(valOne:T, valTwo:U):object {
+    return {valOne, valTwo}
+}
+
+AnotherFunction2(3, {connection: "localhost", username:"123", password:"123"})
+
+const testDatabase:Database = {connection: "localhost", username: "test", password: "test"};
+
+AnotherFunction2(1234, testDatabase)
+
+interface Quiz{
+    name: string,
+    type: string
+}
+
+interface Course{
+    name: string,
+    author: string,
+    subject: string
+}
+
+class Sellable<T>{
+    public cart: T[] = [];
+    addToCart = (products:T) => {
+        this.cart.push(products)
+    }
+}

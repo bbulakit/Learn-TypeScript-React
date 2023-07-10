2818fb4186b8203c3e373845d8680a80687c459e
equinoxza_57@hotmail.com <b.bulakit@hotmail.com>
2023-07-10 20:42:47 +0700

    before useState<Todo> -- take a break

--- 2_React/my-learning-1/src/App.tsx
+++ 2_React/my-learning-1/src/App.tsx
@@ -1,14 +1,19 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import InputField from './assets/components/InputField'
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+// const [first, setFirst] = useState(second)
+  // console.log(todo);
+
   return (
   <div className="App">
     <span className = "heading">Taskify</span>
-    <InputField />
+    <InputField todo={todo} setTodo={setTodo}/>
   </div>
   )
 };
--- 2_React/my-learning-1/src/assets/components/InputField.tsx
+++ 2_React/my-learning-1/src/assets/components/InputField.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import "./styles.css"
-const InputField = () => {
+
+interface Props{
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+// const InputField = ({todo, setTodo}:Props) => { // alternative
+const InputField: React.FC<Props> = ({todo, setTodo}) =>{
   return (
     <form className='input'>
-        <input type="text" placeholder="Enter a task" className="input__box" />        
+        <input type="input" 
+            value = {todo}
+            onChange = { (e) => setTodo(e.target.value)}
+        placeholder="Enter a task" className="input__box" />        
         <button className="input_submit" type="submit">Go</button>
     </form>
   )
